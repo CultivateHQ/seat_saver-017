cd2907026faaf82f45b753bd232c3e6dfc9ea60a
Alan Gardner <urfolomeus@gmail.com>
2016-08-08 17:52:25 +0100

    5. Type annotations

--- web/elm/SeatSaver.elm
+++ web/elm/SeatSaver.elm
@@ -1,8 +1,9 @@
 module SeatSaver exposing (..)
 
-import Html exposing (ul, li, text)
+import Html exposing (..)
 import Html.Attributes exposing (class)
 
+main : Html String
 main =
   view init
 
@@ -18,7 +19,7 @@ type alias Seat =
 type alias Model =
   List Seat
 
-
+init : Model
 init =
   [ { seatNo = 1, occupied = False }
   , { seatNo = 2, occupied = False }
@@ -37,8 +38,10 @@ init =
 
 -- VIEW
 
+view : Model -> Html String
 view model =
   ul [ class "seats" ] (List.map seatItem model)
 
+seatItem : Seat -> Html String
 seatItem seat =
   li [ class "seat available" ] [ text (toString seat.seatNo) ]
